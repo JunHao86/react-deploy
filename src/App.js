12f5ecb27fb3ec0373e1d020b96c71c25267e2da
Emmanuel Lim <106013710+JunHao86@users.noreply.github.com>
2023-07-11 13:44:27 +0800

    Test 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+// npm run deploy 
+// npm install --save gh-pages
+
 function App() {
   return (
     <div className="App">
       <h1>My React App</h1>
       <h2>Author: Emmanuel Lim</h2>
+      <h3>This edit was made before calling npm run deploy</h3>
       {/* <img src={} alt="logo"></img> */}
     </div>
   );
